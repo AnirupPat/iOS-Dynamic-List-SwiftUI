5255eb46f5ebb705fcf68641554cc742f667f157
Anirup Patnaik <anirup049@gmail.com>
2019-11-23 19:45:47 +0530

    first commit

---
+++ DynamicListSwiftUI/Data.swift
@@ -0,0 +1,9 @@
+//
+//  Data.swift
+//  DynamicListSwiftUI
+//
+//  Created by Anirup Patnaik on 23/11/19.
+//  Copyright © 2019 Virtuelabs. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,1 @@
+# iOS-Dynamic-List-SwiftUI
